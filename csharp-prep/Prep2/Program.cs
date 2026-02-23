e1e0ba9d0e30ff757ac932c831b6c8df5f065efc
Oliver Thornton <oliverrt26@hotmail.com>
2026-02-23 09:47:51 -0700

    W01 - Parts 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade %: ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        char letter = 'F';
+
+        if (percentage >= 90)
+        {
+            letter = 'A';
+        } else if (percentage >= 80)
+        {
+            letter = 'B';
+        } else if (percentage >= 70)
+        {
+            letter = 'C';
+        } else if (percentage >= 60)
+        {
+            letter = 'D';
+        }
+
+        Console.WriteLine(letter);
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed, congratulations!");
+        } else
+        {
+            Console.WriteLine("You failed, maybe next time!");
+        }
     }
 }
