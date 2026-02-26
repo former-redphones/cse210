e7b7004f3f886aea4f3071f230f54192b10509e9
Oliver Thornton <oliverrt26@hotmail.com>
2026-02-25 18:35:46 -0700

    W09 - Prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Drawing;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = [
+            new Square("blue", 3),
+            new Rectangle("red", 2, 4),
+            new Circle("green", 8)
+        ];
+        foreach (Shape shape in shapes) {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
+
     }
 }
